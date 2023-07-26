920ea22f054097fef40b9475289b126eecaf16cf
qvantt <2275399@gmail.com>
2023-07-26 17:54:25 +0300

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
