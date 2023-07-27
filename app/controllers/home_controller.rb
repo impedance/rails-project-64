d307e26d924c377a28ae7b8f7bd853d6567ef310
qvantt <2275399@gmail.com>
2023-07-27 12:31:09 +0300

    add devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
+
+  # def destroy
+  #   sign_out(current_user)
+  #   redirect_to root_path, notice: "You have been signed out successfully."
+  # end
 end
